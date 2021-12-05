edf788cf3825a42112a41a897c11c21f6b6d7df2
VanMartynenko <93986955+VanMartynenko@users.noreply.github.com>
2021-12-05 20:52:56 +0200

    Add files via upload

---
+++ head.php
@@ -0,0 +1,17 @@
+<section class="logo">
+	<div class="header-logo">
+		<h1>An English learner</h1>
+	</div>
+</section>	
+<header class="header">
+		<div class="head-line">
+			<ul class="head-nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About us</a></li>
+				<li><a href="#">Contacts</a></li>
+			</ul>
+		<div class="donate">
+			<a href="#">Donate</a>
+		</div>
+	</div>
+</header>
